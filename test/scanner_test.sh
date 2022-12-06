128dc8c9c6e5a04dc000cc5bd5e9beca52f85b77
Vladimir284 <xmecia00@stud.fit.vutbr.cz>
2022-12-06 17:10:51 +0100

    Script hotfix

--- test/scanner_test.sh
+++ test/scanner_test.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-BIN="ial22"
+# All files
 filename_array=("graph_16_cell" "graph_antenna" "graph_antiprism" "graph_Chvatal" "graph_cross" "graph_Franklin" "graph_GoddardHenning" "graph_Grotzsch" "graph_Heawood" "graph_K3,3" "graph_K5" "graph_Lemke" "graph_Petersen" "graph_sun" "graph_Tilley")
-#file_array=("$(cat input/graph_16_cell.graph)" "$(cat input/graph_antenna.graph)" "$(cat input/graph_antiprism.graph)" "$(cat input/graph_Chvatal.graph)" "$(cat input/graph_cross.graph)" "$(cat input/graph_Franklin.graph)" "$(cat input/graph_GoddardHenning.graph)" "$(cat input/graph_Grotzsch.graph)" "$(cat input/graph_Heawood.graph)")
 
-#echo "\nTest output differences:"
-#echo "Test ${filename_array[$j]}"
-#./ial22 scanner <"input/${filename_array[j]}.graph" >"${filename_array[j]}_current.output"
-#diff -s -U 5 "${filename_array[j]}_current.output" "test/scanner/${filename_array[j]}_reference.output"
-#rm "${filename_array[j]}_current.output"
-#exit 0
-
-
+# Iterate through each one and compare results
 for (( j=0; j<${#filename_array[@]}; j++ ))
 do
   echo "Test output differences:"
